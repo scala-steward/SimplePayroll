5b2d2593b6d37e4ef89d6210801a1fcbc6098613
Scala Steward <me@scala-steward.org>
2020-05-10 16:14:39 +0200

    Update newtype to 0.4.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   lazy val fs2 = "co.fs2" %% "fs2-core" % "2.2.2"
   lazy val meowMtlCore = "com.olegpy" %% "meow-mtl-core" % "0.4.0"
   lazy val meowMtlEffects = "com.olegpy" %% "meow-mtl-effects" % "0.4.0"
-  lazy val newType = "io.estatico" %% "newtype" % "0.4.3"
+  lazy val newType = "io.estatico" %% "newtype" % "0.4.4"
   lazy val refined = "eu.timepit" %% "refined" % "0.9.12"
   lazy val monocleCore = "com.github.julien-truffaut" %% "monocle-core" % "2.0.1"
   lazy val monocleMacro = "com.github.julien-truffaut" %% "monocle-macro" % "2.0.1"
